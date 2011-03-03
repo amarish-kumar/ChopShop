83bcb10f10780704d6879add15cf314078da7ed2
Reza <reza.mahmood@gmail.com>
2011-03-03 21:06:05 +0000

    Added Product class

---
+++ ChopShop.Model/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChopShop.Model
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+    }
+}
