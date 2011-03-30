1985532229d80e9b552e3cf5f0e619cc143d2388
Chris Himsworth <chrishims@hotmail.com>
2011-03-30 21:48:34 +0100

    US4 changes

--- ChopShop.Admin.Services/ProductService.cs
+++ ChopShop.Admin.Services/ProductService.cs
@@ -53,6 +53,7 @@ namespace ChopShop.Admin.Services
                                                  .Add(Restrictions.Eq("Id", productId));
             var product = repository.Search(searchCriteria).FirstOrDefault();
             product.IsDeleted = true;
+            product.Quantity = 0;
             repository.Update(product);
             return true;
         }
