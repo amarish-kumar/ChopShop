bdfca946a997906dec213f11a587ec40494946cb
Reza Mahmood <reza.mahmood@gmail.com>
2011-04-06 14:19:38 +0100

    sorting out branches

---
+++ ChopShop.Model/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace ChopShop.Model
+{
+    public class Customer : Entity
+    {
+        public virtual string FirstName { get; set; }
+        public virtual string LastName { get; set; }
+        public virtual Address BillingAddress { get; set; }
+        public virtual Address DeliveryAddress { get; set; }
+        public virtual string ContactNumber { get; set; }
+    }
+}
---
+++ ChopShop.Model/Order.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace ChopShop.Model
+{
+    public class Order : Entity
+    {
+        public virtual Customer Customer { get; set; }
+        public virtual Address BillingAddress { get; set; }
+        public virtual Address DeliveryAddress { get; set; }
+        public virtual ICollection<OrderItem> OrderItems { get; set; }
+    }
+}
